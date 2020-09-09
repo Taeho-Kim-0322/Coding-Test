cc7dbd469ba1fb63b39b638507513f63f6ae6c6a
taeho <kimtaeho0322@gmail.com>
2020-09-09 18:44:10 +0900

    2020_09_09_1844

--- 20200909_realization.py
+++ 20200909_realization.py
@@ -1,3 +1,28 @@
+#####실전 2. 왕실의 나이트
+
+n = input()
+#ord 함수 생각...
+x , y = int(ord(n[0])) - int(ord('a')) + 1, int(n[1])
+
+#갈 수 있는 8가지 방법 다 정해놓고...
+#8개 다 해보면서 가능하면 cnt +
+steps = [(-2,-1), (-2, 1), (1, 2), (-1, 2), (2, 1), (2, -1), (-1, -2), (1, -2)]
+answer = 0
+for case in steps:
+    nx = x + case[0]
+    ny = y + case[1]
+
+    if nx < 1 or nx > 8 or ny < 1 or ny > 8:
+        continue
+    else:
+        answer += 1
+
+print(answer)
+
+
+
+
+
 ######### 예제 4-1 상하좌우
 '''
 n = int(input())
@@ -26,7 +51,7 @@ print(nx, ny)
 
 
 ######### 예제 4-2 시각
-
+'''
 n = int(input())
 count = 0
 
@@ -39,3 +64,4 @@ for i in range(n+1):
 
 print(count)
 
+'''
